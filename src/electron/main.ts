9bd85332ca8e363250b24cfb04c5bc98a3c366dd
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-08-10 04:23:30 +0530

    fix: electron builder and scripts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -28,7 +28,7 @@ function createWindow() {
   }
 
   mainWindow.removeMenu()
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', () => {
     mainWindow.destroy()
